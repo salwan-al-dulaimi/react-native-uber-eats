a4a86b816286096a0f06ef9afce18ce91671d9c3
Salwan-Aldulaimi <salwan@holygrow.nl>
2022-07-25 23:03:24 +0200

    Add Yalp key

--- App.js
+++ App.js
@@ -1,22 +1,27 @@
+import React from "react";
 import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import Home from "./screens/Home";
 import Screen from "./components/Screen";
 import HeaderTabs from "./components/HeaderTabs";
 import SearchBar from "./components/SearchBar";
 import Categories from "./components/Categories";
-import RestaurantItem from "./components/RestaurantItem";
+import RestaurantItems from "./components/RestaurantItems";
+import YalpKey from "./.env";
 
 export default function App() {
+  const [YalpKey, setYalpKey] = React.useState([YalpKey]);
+  console.log(YalpKey);
   return (
-    <Screen style={{ backgroundColor: "#eee", flex: 1 }}>
+    < Screen style={{ backgroundColor: "#eee", flex: 1 }
+    }>
       <View style={{ backgroundColor: "white", padding: 15 }}>
         <HeaderTabs />
         <SearchBar />
       </View>
       <ScrollView showsVerticalScrollIndicator={false}>
         <Categories />
-        <RestaurantItem />
+        <RestaurantItems />
       </ScrollView>
-    </Screen>
+    </Screen >
   );
 }
